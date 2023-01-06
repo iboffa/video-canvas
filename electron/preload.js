8a9f7363b82eb50b54235286af77f46729c388bc
iboffa <i.boffa@hotmail.it>
2023-01-06 11:10:18 +0100

    preload written in typescript

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('record', {
-  start: () => ipcRenderer.send('start-recording'),
-  sendChunk: (chunk) => {
-    ipcRenderer.send('video-chunk', chunk)
-  },
-  stop: () => ipcRenderer.send('stop-recording')
-})
